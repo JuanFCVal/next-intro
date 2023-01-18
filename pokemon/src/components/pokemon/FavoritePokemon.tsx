21032fd668a036ec6e18db6f39f7a675fcb197ae
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2023-01-17 21:47:48 -0500

    :sparkles: navigate to pokemon on fav

--- pokemon/src/components/pokemon/FavoritePokemon.tsx
+++ pokemon/src/components/pokemon/FavoritePokemon.tsx
@@ -1,12 +1,18 @@
 import { Grid, Card } from '@nextui-org/react'
+import { useRouter } from 'next/router'
 import React, { FC } from 'react'
 
 interface FavoritePokemonProps {
   id: number
 }
 const FavoritePokemonCard: FC<FavoritePokemonProps> = ({ id }) => {
+  const router = useRouter()
+  const OnFavoriteClick = () => {
+    console.log('Favorite Clicked', id)
+    router.push(`/pokemon/${id}`)
+  }
   return (
-    <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+    <Grid xs={6} sm={3} md={2} xl={1} key={id} onClick={OnFavoriteClick}>
       <Card isHoverable>
         <Card.Image
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
