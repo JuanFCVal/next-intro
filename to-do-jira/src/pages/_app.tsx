063664ab46d24f897a0731f7ac3bb884af5bc631
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2023-01-18 08:19:25 -0500

    :sparkles: create styles themes

--- to-do-jira/src/pages/_app.tsx
+++ to-do-jira/src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { lightbasicTheme, darkBasicTheme } from '../../themes'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightbasicTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
--- to-do-jira/src/pages/index.tsx
+++ to-do-jira/src/pages/index.tsx
@@ -1,10 +1,13 @@
+import { Typography } from '@mui/material'
 import { NextPage } from 'next'
 import React from 'react'
 
 const HomePage: NextPage = () => {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Typography variant='h1' color='primary'>
+        Hola
+      </Typography>
     </div>
   )
 }
