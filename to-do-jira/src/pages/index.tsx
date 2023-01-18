2c2ada13a0cd22bc9fc06dfee5cf07af3b87175b
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2023-01-18 07:37:59 -0500

    :tada: init project

---
+++ to-do-jira/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+const HomePage: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default HomePage
